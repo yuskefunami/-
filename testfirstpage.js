a585ef8d8c9b73736b440b3091691736c6a993be
yuskefunami <88821247+yuskefunami@users.noreply.github.com>
2021-09-08 18:51:37 +0900

    Create testfirstpage.js

---
+++ testfirstpage.js
@@ -0,0 +1,38 @@
+
+$(function() {
+  //最初の顔が表示される機能
+
+
+  
+  //序盤のコメント
+
+  setTimeout(() => {
+    $('#a').fadeIn();
+  },1500);
+  setTimeout(() => {
+    $('#a').fadeOut();
+  },3000);
+   setTimeout(() => {
+    $('#frontface').fadeIn();
+  },1500);
+   setTimeout(() => {
+    $('#c').fadeIn();
+  },4500);
+  setTimeout(() => {
+    $('#c').fadeOut();
+  },5000);
+  setTimeout(() => {
+    $('#d').fadeIn();
+  },6500);
+  setTimeout(() => {
+    $('.button-container').show();
+  },6500);
+
+
+
+
+
+
+});
+
+
