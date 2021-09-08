94531301f7d164cfa2f68d86dae1b1daad1601cd
yuskefunami <88821247+yuskefunami@users.noreply.github.com>
2021-09-08 18:51:55 +0900

    Create arrowfunction.js

---
+++ arrowfunction.js
@@ -0,0 +1,59 @@
+$(function(    ) {
+    //ランダムに顔画像を出力
+
+    const imageNum = document.getElementById("randomImage");
+    const images = ["./img/S__8790042.jpg","./img/S__8790044.jpg","./img/S__8790045.jpg","./img/S__8790046.jpg"]
+    const imageNo = Math.floor(Math.random() * images.length);
+    
+    $(".btn").click(function(){
+
+        imageNum.src = images[imageNo];
+        
+        $("#frontface").hide();
+        $("#randomImage").show();
+       
+
+        }); 
+    
+    //押したボタンによって、正誤評価する。
+    $("#left").click(function(){
+        if(imageNo == 1){
+            $("#randomImage").fadeOut(2000);
+            $(".button-container").fadeOut(2000);
+            $("#winnerface").show(10000);
+            $("#win").fadeIn(8000);
+        }else{
+            $("#lose").fadeIn(500);
+        }
+    });
+    $("#right").click(function(){
+        if(imageNo == 3){
+            $("#randomImage").fadeOut(2000);
+            $(".button-container").fadeOut(2000);
+            $("#winnerface").show(10000);
+            $("#win").fadeIn(8000);
+        }else{
+            $("#lose").fadeIn(500);
+        }
+    });
+    $("#up").click(function(){
+        if(imageNo == 2){
+            $("#randomImage").fadeOut(2000);
+            $(".button-container").fadeOut(2000);
+            $("#winnerface").show(10000);
+            $("#win").fadeIn(8000);
+        }else{
+            $("#lose").fadeIn(500);
+        }
+    });
+    $("#down").click(function(){
+        if(imageNo == 0){
+            $("#randomImage").fadeOut(2000);
+            $(".button-container").fadeOut(2000);
+            $("#winnerface").show(10000);
+            $("#win").fadeIn(8000);
+        }else{
+            $("#lose").fadeIn(500);
+        }
+    });
+});
